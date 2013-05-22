79167d3e35c902cb979976c4a642a194088f855b
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2013-05-22 09:40:07 -0400

    Added solution/project with initial implementation.

---
+++ src/Ninject.Extensions.Wcf.Client/NinjectWcfClientExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using Ninject;
+using Ninject.Syntax;
+
+namespace Ninject.Extensions.Wcf.Client
+{
+    public static class NinjectWcfClientExtensions
+    {
+        public static IBindingWhenInNamedWithOrOnSyntax<T> ToServiceChannel<T>(this IBindingToSyntax<T> syntax,
+                                                                               string endpointConfigurationName = null)
+        {
+            BindChannelFactory<T>(syntax.Kernel, endpointConfigurationName);
+            return syntax.ToMethod(x => syntax.Kernel.Get<ChannelFactory<T>>(endpointConfigurationName).CreateChannel());
+        }
+
+        private static void BindChannelFactory<T>(IKernel kernel, string endpointConfigurationName)
+        {
+            var factoryBinding = kernel.Bind<ChannelFactory<T>>().ToSelf().InSingletonScope();
+            if (endpointConfigurationName != null)
+            {
+                factoryBinding.Named(endpointConfigurationName);
+            }
+            factoryBinding.WithConstructorArgument("endpointConfigurationName", endpointConfigurationName);
+        }
+    }
+}
